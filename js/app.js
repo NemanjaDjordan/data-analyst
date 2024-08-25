e90638b2d86569322084fd621cfb8d35d5919d22
NemanjaDjordan <49299799+NemanjaDjordan@users.noreply.github.com>
2024-08-25 20:50:55 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,101 +1,15 @@
-// // Data for the first donut chart
-// const data1 = {
-//     labels: ['Canada', 'England', 'France', 'Germany', 'India', 'Italy', 'Japan', 'USA'],
-//     datasets: [{
-//         data: [9.02, 14.30, 14.93, 12.07, 10.50, 12.16, 15.54, 11.48],
-//         backgroundColor: [
-//             '#FF6384',
-//             '#36A2EB',
-//             '#FFCE56',
-//             '#4BC0C0',
-//             '#9966FF',
-//             '#FF9F40',
-//             '#FFCD56',
-//             '#4D5360'
-//         ],
-//         hoverOffset: 4
-//     }]
-// };
-
-// // Data for the second donut chart (random data)
-// const data2 = {
-//     labels: ['Brazil', 'China', 'Russia', 'South Africa', 'Australia'],
-//     datasets: [{
-//         data: [20, 25, 15, 10, 30],
-//         backgroundColor: [
-//             '#FF6384',
-//             '#36A2EB',
-//             '#FFCE56',
-//             '#4BC0C0',
-//             '#9966FF'
-//         ],
-//         hoverOffset: 4
-//     }]
-// };
-
-// // Chart options
-// const options = {
-//     cutout: '70%',
-//     responsive: true,
-//     plugins: {
-//         legend: {
-//             display: false
-//         },
-//         tooltip: {
-//             callbacks: {
-//                 label: function(tooltipItem) {
-//                     return tooltipItem.label + ': ' + tooltipItem.raw + '%';
-//                 }
-//             }
-//         }
-//     }
-// };
-
-// // Create the first donut chart
-// const ctx1 = document.getElementById('donutChart1').getContext('2d');
-// const donutChart1 = new Chart(ctx1, {
-//     type: 'doughnut',
-//     data: data1,
-//     options: options
-// });
-
-// // Create the second donut chart
-// const ctx2 = document.getElementById('donutChart2').getContext('2d');
-// const donutChart2 = new Chart(ctx2, {
-//     type: 'doughnut',
-//     data: data2,
-//     options: options
-// });
-
-// // Generate legend for the first donut chart
-// const legendContainer1 = document.getElementById('legend1');
-// data1.labels.forEach((label, index) => {
-//     const color = data1.datasets[0].backgroundColor[index];
-//     const value = data1.datasets[0].data[index];
-//     const legendItem = document.createElement('div');
-//     legendItem.innerHTML = `<span style="background-color:${color}"></span>${label} (${value}%)`;
-//     legendContainer1.appendChild(legendItem);
-// });
-
-// // Generate legend for the second donut chart
-// const legendContainer2 = document.getElementById('legend2');
-// data2.labels.forEach((label, index) => {
-//     const color = data2.datasets[0].backgroundColor[index];
-//     const value = data2.datasets[0].data[index];
-//     const legendItem = document.createElement('div');
-//     legendItem.innerHTML = `<span style="background-color:${color}"></span>${label} (${value}%)`;
-//     legendContainer2.appendChild(legendItem);
-// });
-
-
-
-
+// Select elements
 const navigationMenu = document.querySelector('.navigation-menu');
 const body = document.querySelector('body');
 const burgerWrap = document.querySelector('.burgerwrap');
 const jsNavigation = document.querySelectorAll('.js-navigation');
+const portfolioItem = document.querySelectorAll('.raw-files .item');
+const heroAnimationElements = document.querySelectorAll('.section-hero .wrapper .top, .section-hero .wrapper .middle, .section-hero .wrapper .bottom');
+const blobCodeLines = document.querySelectorAll('.blob-code-inner');
+const portfolioButtons = document.querySelectorAll('.button');
+const tableCells = document.querySelectorAll('td');
 
-// Reusable functions to avoid code repetition
+// Utility functions
 function toggleScrollLock() {
     body.classList.toggle('no-scroll');
 }
@@ -104,20 +18,37 @@ function removeScrollLock() {
     body.classList.remove('no-scroll', 'body-blur');
 }
 
-//Burger event handler
-burgerWrap.addEventListener("click", function () {
+function highlightTextContent(element, regex, color) {
+    const html = element.innerHTML;
+    element.innerHTML = html.replace(regex, `<span style="color: ${color}; display:inline-block">$&</span>`);
+}
+
+function handleNavigation(evt) {
+    evt.preventDefault();
+    const dataId = this.getAttribute('data-id');
+    const targetElement = document.querySelector(`[data-name="${dataId}"]`);
+    if (targetElement) {
+        const offset = window.innerHeight * 0.02;
+        window.scrollTo({
+            top: targetElement.offsetTop - offset,
+            behavior: 'smooth'
+        });
+    }
+}
+
+// Burger menu event handler
+burgerWrap.addEventListener("click", () => {
     burgerWrap.classList.toggle('burger-animation');
     navigationMenu.classList.toggle('navigation-menu-animation');
     toggleScrollLock();
 });
 
-// Escape key event handler for closing the menu
-document.addEventListener('keyup', function (evt) {
-    if (evt.keyCode === 27) {  // 27 is the keyCode for the Escape key
-        if (burgerWrap.classList.contains('burger-animation')) {
-            burgerWrap.classList.remove('burger-animation');
-            navigationMenu.classList.remove('navigation-menu-animation');
-        }
+// Escape key event handler
+document.addEventListener('keyup', evt => {
+    if (evt.key === 'Escape') {
+        burgerWrap.classList.remove('burger-animation');
+        navigationMenu.classList.remove('navigation-menu-animation');
+        portfolioItem.forEach(item => item.classList.remove('portfolio-active'));
         removeScrollLock();
     }
 });
@@ -128,24 +59,74 @@ jsNavigation.forEach(element => {
     element.addEventListener("touchstart", handleNavigation);
 });
 
-function handleNavigation(evt) {
-    evt.preventDefault();
-    const dataId = this.getAttribute('data-id');
-    const windowsHeight = window.innerHeight * 0.02;
-    const targetElement = document.querySelector(`[data-name="${dataId}"]`);
-    window.scrollTo({
-        top: targetElement.offsetTop - windowsHeight,
-        behavior: 'smooth'
-    });
-}
-
-
 // Section hero animation
-document.addEventListener('DOMContentLoaded', function() {
-    const heroAnimationElements = document.querySelectorAll('.section-hero .wrapper .top, .section-hero .wrapper .middle, .section-hero .wrapper .bottom');
-    
+document.addEventListener('DOMContentLoaded', () => {
     heroAnimationElements.forEach(element => {
         element.classList.add('section-hero-animate');
     });
 });
 
+// Portfolio beautification
+const keywords = ['sns', 'plt', 'pandas', 'matplotlib', 'pyplot', 'seaborn', 'pd'];
+const words = ['file_path', 'df', 'figsize', 'segment_sales', 'data', 'palette', 'rotation', 'country_sales', 'customer_sales', 'monthly_profit', 'marker', 'units_sold_split', 'top_product'];
+
+blobCodeLines.forEach(line => {
+    const containsHash = line.textContent.includes('#');
+    const parenColor = containsHash ? '#6a993e' : '#ce9178';
+    const keywordColor = containsHash ? '#6a993e' : '#3ac9a2';
+    const wordColor = containsHash ? '#6a993e' : '#9cdcfe';
+    const quoteColor = containsHash ? '#6a993e' : '#ce9178';
+
+    let html = line.innerHTML
+        .replace(/\(/g, `<span style="color: ${parenColor}; display:inline-block">(</span>`)
+        .replace(/\)/g, `<span style="color: ${parenColor}; display:inline-block">)</span>`)
+        .replace(/\bimport\b/g, '<span style="color: #c586c0; display:inline-block">import</span>');
+
+    keywords.forEach(keyword => {
+        highlightTextContent(line, new RegExp(`\\b${keyword}\\b`, 'g'), keywordColor);
+    });
+
+    words.forEach(word => {
+        highlightTextContent(line, new RegExp(`\\b${word}\\b`, 'g'), wordColor);
+    });
+
+    html = html.replace(/'([^']*)'/g, match => `<span style="color: ${quoteColor}; display:inline-block">${match}</span>`);
+    line.innerHTML = html;
+});
+
+// Highlight '#' in table cells
+tableCells.forEach(td => {
+    if (td.textContent.includes('#')) {
+        td.style.color = '#6a993e';
+    }
+});
+
+// Portfolio buttons
+portfolioButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const id = this.getAttribute('data-id');
+        const correspondingItem = document.querySelector(`.item[data-id="${id}"]`);
+        if (correspondingItem) {
+            correspondingItem.classList.toggle('portfolio-active');
+        }
+
+        // Check if any item has the 'portfolio-active' class
+        const isActive = Array.from(portfolioItem).some(item => item.classList.contains('portfolio-active'));
+        burgerWrap.style.display = isActive ? 'none' : 'inline-block';
+    });
+});
+
+// Add event listener to close buttons
+document.querySelectorAll('.close-button').forEach(button => {
+    button.addEventListener('click', function() {
+        // Get the parent element with the 'item' class
+        const correspondingItem = this.closest('.item');
+        if (correspondingItem) {
+            correspondingItem.classList.toggle('portfolio-active');
+        }
+
+        // Check if any item has the 'portfolio-active' class
+        const isActive = Array.from(portfolioItem).some(item => item.classList.contains('portfolio-active'));
+        burgerWrap.style.display = isActive ? 'none' : 'inline-block';
+    });
+});
